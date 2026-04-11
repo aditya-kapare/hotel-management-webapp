bd5f37a15f7a465f91e64e5607e8835ddc4820c3
Aditi Manohar Mane <aditi.mane@zensar.com>
2026-04-11 16:44:58 +0530

    Added Multiple Filters and Formatting in room view

--- HotelManagement/HotelManagement.WebApp/Controllers/RoomsController.cs
+++ HotelManagement/HotelManagement.WebApp/Controllers/RoomsController.cs
@@ -15,15 +15,49 @@ namespace HotelManagement.WebApp.Controllers
         }
 
         // Common: Admin + Receptionist
-        public async Task<IActionResult> Index(RoomType? type)
+        //public async Task<IActionResult> Index(string? type)
+        //{
+        //    var rooms = string.IsNullOrEmpty(type)
+        //        ? await _admin.Rooms.GetAllAsync()
+        //        : Enum.TryParse<RoomType>(type, out var parsed)
+        //            ? await _admin.Rooms.GetByTypeAsync(parsed)
+        //            : await _admin.Rooms.GetAllAsync();
+
+        //    // Prepare UI data (no enum exposure to view)
+        //    ViewBag.RoomTypes = rooms
+        //        .Select(r => r.RoomType.ToString())
+        //        .Distinct()
+        //        .ToList();
+
+        //    ViewBag.SelectedType = type;
+
+        //    return View(rooms);
+        //}
+
+        public async Task<IActionResult> Index(
+        string? type,
+        string? acOption,
+        string? cleanStatus,
+        string? availabilityStatus)
         {
-            var rooms = type is null
-                ? await _admin.Rooms.GetAllAsync()
-                : await _admin.Rooms.GetByTypeAsync(type.Value);
+            var rooms = await _admin.Rooms.GetAllAsync();
+
+            if (!string.IsNullOrEmpty(type))
+                rooms = rooms.Where(r => r.RoomType.ToString() == type).ToList();
+
+            if (!string.IsNullOrEmpty(acOption))
+                rooms = rooms.Where(r => r.AcOption.ToString() == acOption).ToList();
+
+            if (!string.IsNullOrEmpty(cleanStatus))
+                rooms = rooms.Where(r => r.CleanStatus.ToString() == cleanStatus).ToList();
+
+            if (!string.IsNullOrEmpty(availabilityStatus))
+                rooms = rooms.Where(r => r.AvailabilityStatus.ToString() == availabilityStatus).ToList();
+
+            PrepareFilters(rooms, type, acOption, cleanStatus, availabilityStatus);
 
             return View(rooms);
         }
-
         // Admin only
         public IActionResult Create()
         {
@@ -89,6 +123,29 @@ namespace HotelManagement.WebApp.Controllers
         {
             await _admin.Rooms.DeleteAsync(id);
             return RedirectToAction(nameof(Index));
+        }
+
+        private void PrepareFilters(
+         IEnumerable<RoomDto> rooms,
+         string? type,
+         string? acOption,
+         string? cleanStatus,
+         string? availabilityStatus)
+        {
+            ViewBag.RoomTypes = rooms
+                .Select(r => r.RoomType.ToString())
+                .Distinct()
+                .OrderBy(x => x)
+                .ToList();
+
+            ViewBag.AcOptions = new[] { "AC", "NonAC" };
+            ViewBag.CleanStatuses = new[] { "Clean", "Dirty" };
+            ViewBag.AvailabilityStatuses = new[] { "Available", "Occupied" };
+
+            ViewBag.SelectedType = type;
+            ViewBag.SelectedAc = acOption;
+            ViewBag.SelectedClean = cleanStatus;
+            ViewBag.SelectedAvailability = availabilityStatus;
         }
     }
 }
