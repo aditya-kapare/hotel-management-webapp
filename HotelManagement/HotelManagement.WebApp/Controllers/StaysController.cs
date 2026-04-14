c1a593bbb26788ba1adb5274d4b4daf138dbe41b
Aditya Shailesh Kapare <aditya.kapare@zensar.com>
2026-04-14 14:17:09 +0530

    Changed Stay presentation to not use jQuery for browser side validation

--- HotelManagement/HotelManagement.WebApp/Controllers/StaysController.cs
+++ HotelManagement/HotelManagement.WebApp/Controllers/StaysController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HotelManagement.WebApp.Controllers
 {
-    [Authorize(Roles = "Receptionist")]
+    //[Authorize(Roles = "Receptionist")]
     [Route("stays")]
     public sealed class StaysController : Controller
     {
